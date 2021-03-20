ce6a3eee239f9fea35acfcc84eada8b41bf86b98
LazyMan78 <demetra_n@front.ru>
2021-03-20 12:49:55 +0300

    setup admin

--- src/products/admin.py
+++ src/products/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from .models import Product
 # Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["__str__", "description", "price"]
+    list_filter = ["price"]
+    search_fields = ["title", "description"]
+    list_editable = ["price"]
+    class Meta:
+        model = Product
+
+admin.site.register(Product, ProductAdmin)
--- src/products/models.py
+++ src/products/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+    price = models.DecimalField(max_digits=100, decimal_places=2, default=9.99)
+
+    def __str__(self):
+        return self.title

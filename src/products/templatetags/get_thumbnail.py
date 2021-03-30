888005f2d784b01a6c816f73808145528c762dae
LazyMan78 <demetra_n@front.ru>
2021-03-30 20:41:43 +0300

    create custom templatefilter

--- src/products/templatetags/get_thumbnail.py
+++ src/products/templatetags/get_thumbnail.py
@@ -1,6 +1,14 @@
+from django import template
+from ..models import Product, THUMB_CHOICES
+register = template.Library()
 
-
+@register.filter
 def get_thumbnail(obj, arg):
+    arg = arg.lower()
+    if not isinstance(obj, Product):
+        raise TypeError(f"{obj} is not valid for this model")
+    choises = dict(THUMB_CHOICES)
+    if not choises.get(arg):
+        raise TypeError(f"{arg} is not valid type for this model")
 
-
-    return obj
+    return obj.thumbnail_set.filter(type=arg).first().media.url
